76efa5a1cf18d36618fe5e71f100b5eb3bfb1490
Matt Breton <bretonstyle@gmail.com>
2023-01-09 00:09:24 -0500

    Add colors upon pickup

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -6,11 +6,17 @@ public class Delivery : MonoBehaviour
 {
     Driver playerOne;
     
+    [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32 (1, 1, 1, 1);
     [SerializeField] bool hasPackage = false;
     [SerializeField] float destroyDelay = 0.5f;
+
+    SpriteRenderer spriteRenderer;
+    
     void Start()
     {
         playerOne = FindObjectOfType<Driver>().GetComponent<Driver>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Whoops!");
@@ -19,6 +25,7 @@ public class Delivery : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Package" && !hasPackage)
         { 
+            spriteRenderer.color= hasPackageColor;
             Destroy(other.gameObject, destroyDelay);
             Debug.Log("Package Picked Up!");
             hasPackage = true;
@@ -26,6 +33,7 @@ public class Delivery : MonoBehaviour
         else if (other.tag == "Customer" && hasPackage == true)
         {
             Debug.Log("Delivered ze Package!");
+            spriteRenderer.color= noPackageColor;
             hasPackage = false;
             playerOne.SetScore(playerOne.GetScore() + 1);
             Debug.Log("New score ==" + playerOne.GetScore());
